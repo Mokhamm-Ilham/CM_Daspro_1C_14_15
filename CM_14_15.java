da8deb0181e7c51df4d4fede63c176d218617e71
Hamm <ilhampolpro45@gmail.com>
2024-12-10 11:48:20 +0700

    Tahap Awal

--- CM_14_15.java
+++ CM_14_15.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class CM_14_15 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int menu = 0;
+        do {
+            System.out.println("===== MENU INVENTORI KAFE =====");
+            System.out.println("1. Tampilkan Inventori");
+            System.out.println("2. Tambah Stok untuk Item yang Ada");
+            System.out.println("3. Tambah Item Baru");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih Menu: ");
+            menu = sc.nextInt();
+            if (menu == 1) {
+                
+            } else if (menu == 2) {
+                
+            } else if (menu == 3) {
+                
+            } 
+            
+        } while (menu==4);
     }
 }
