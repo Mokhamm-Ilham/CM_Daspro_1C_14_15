3eda50e596e612834c00c128beb7fa7938e52606
Hamm <ilhampolpro45@gmail.com>
2024-12-10 16:55:51 +0700

    15_Tambah Stok

--- CM_14_15.java
+++ CM_14_15.java
@@ -7,27 +7,35 @@ public class CM_14_15 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int menu = 0;
+        int pilihMenu = 0;
         do {
-            System.out.println("===== MENU INVENTORI KAFE =====");
-            System.out.println("1. Tampilkan Inventori");
-            System.out.println("2. Tambah Stok untuk Item yang Ada");
-            System.out.println("3. Tambah Item Baru");
-            System.out.println("4. Keluar");
+            menu();
             System.out.print("Pilih Menu: ");
-            menu = sc.nextInt();
-            if (menu == 1) {
+            pilihMenu = sc.nextInt();
+            if (pilihMenu == 1) {
                 tampilkanInventori();
-            } else if (menu == 2) {
-                
-            } else if (menu == 3) {
+                System.out.println();
+            } else if (pilihMenu == 2) {
+                tambahStok();
+                System.out.println();
+            } else if (pilihMenu == 3) {
                 tambahItem();
-            } 
-            
-        } while (menu!=4);
+                System.out.println();
+            }
+
+        } while (pilihMenu != 4);
+    }
+
+    static void menu(){
+        System.out.println("===== MENU INVENTORI KAFE =====");
+        System.out.println("1. Tampilkan Inventori");
+        System.out.println("2. Tambah Stok untuk Item yang Ada");
+        System.out.println("3. Tambah Item Baru");
+        System.out.println("4. Keluar");
     }
 
     static void tampilkanInventori() {
+        System.out.println();
         System.out.println("===== DATA INVENTORI =====");
         System.out.println("No\tNama Item\tKategori\tStok");
         System.out.println("--------------------------------------------");
@@ -37,22 +45,51 @@ public class CM_14_15 {
             }
             System.out.print((i + 1) + "\t");
             for (int j = 0; j < item[i].length; j++) {
-                System.out.print(item[i][0] + " \t");
+                System.out.print(item[i][j] + " \t");
             }
             System.out.print(stok[i] + "\n");
         }
     }
 
+    static int tambahStok() {
+        sc.nextLine();
+        int no, tambahStok, hasil;
+        System.out.println("===== TAMBAH STOK =====");
+        do {
+            System.out.print("Masukkan nomor Item: ");
+            no = sc.nextInt();
+            if (no == 0 || no > item.length) {
+                System.out.println("Item tidak ditemukan");
+            }
+        } while (no==0 || no > item.length);
+        do {
+            System.out.print("Masukkan jumlah stok yang ingin ditambahkan: ");
+            tambahStok = sc.nextInt();
+            if (tambahStok == 0) {
+                System.out.println("Stok tidak boleh 0");
+            }
+        } while (tambahStok == 0);
+        hasil = stok[no - 1] += tambahStok;
+        System.out.println("Stok "+item[no-1][0]+" berhasil ditambah. Stok sekarang: "+hasil);
+        return hasil;
 
+    }
 
     static void tambahItem() {
+        sc.nextLine();
+        int jumlahStok=0;
         System.out.println("===== TAMBAH ITEM BARU =====");
-        System.out.print("Masukkan Nama Item: ");
+        System.out.print("Masukkan nama item baru: ");
         String nama = sc.nextLine();
-        System.out.print("Masukkan Kategori: ");
+        System.out.print("Masukkan kategori item baru: ");
         String kategori = sc.nextLine();
-        System.out.print("Masukkan Stok: ");
-        int jumlahStok = sc.nextInt();
+        do {
+            System.out.print("Masukkan jumlah stok awal: ");
+            jumlahStok = sc.nextInt();
+            if (jumlahStok == 0) {
+                System.out.println("Stok tidak boleh 0");
+            }
+        } while (jumlahStok == 0);
         sc.nextLine();
         String[][] itemBaru = new String[item.length + 1][2];
         int[] stokBaru = new int[stok.length + 1];
@@ -62,12 +99,12 @@ public class CM_14_15 {
             stokBaru[i] = stok[i];
         }
 
-        itemBaru[item.length] = new String[]{nama, kategori};
+        itemBaru[item.length] = new String[] { nama, kategori };
         stokBaru[stok.length] = jumlahStok;
 
         item = itemBaru;
         stok = stokBaru;
+        System.out.println("Item baru berhasil ditambahkan: "+item[item.length-1][0] + " ("+item[item.length-1][1]+") - Stok: " + stok[stok.length-1]);
     }
-
 
 }
