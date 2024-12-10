36cf233bf46c374b6141c17e15b65a1f6fd28094
arxsyy <marsyfernanda@gmail.com>
2024-12-10 14:33:07 +0700

    14_Tampil Inventori

--- CM_14_15.java
+++ CM_14_15.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class CM_14_15 {
+    static int[] stok = new int[10];
+    static String[][] item = new String[10][2];
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int menu = 0;
@@ -13,7 +17,7 @@ public class CM_14_15 {
             System.out.print("Pilih Menu: ");
             menu = sc.nextInt();
             if (menu == 1) {
-                
+                tampilkanInventori();
             } else if (menu == 2) {
                 
             } else if (menu == 3) {
@@ -22,4 +26,22 @@ public class CM_14_15 {
             
         } while (menu==4);
     }
+
+    static void tampilkanInventori() {
+        System.out.println("===== DATA INVENTORI =====");
+        System.out.println("No\tNama Item\tKategori\tStok");
+        System.out.println("--------------------------------------------");
+        for (int i = 0; i < item.length; i++) {
+            if (item[i][0] == null) {
+                break;
+            }
+            System.out.print((i + 1) + "\t");
+            for (int j = 0; j < item[i].length; j++) {
+                System.out.print(item[i][0] + "\t");
+            }
+            System.out.print(stok[i] + "\n");
+        }
+    }
+
+
 }
